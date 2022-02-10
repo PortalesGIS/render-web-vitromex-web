52279ef898329118b679adb0b291c6a6b9dce4e5
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2022-02-10 17:30:54 -0600

    feat: landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import logovitromex from "../assets/logovitromex.svg";
+import { Path } from "../utils/route";
 
 export const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <div className="bg-neutral grid-cols-movil tablet:grid-cols-tableta desktop:grid-cols-desktop relative grid h-screen w-screen grid-rows-2 px-4">
+      <div className="tablet:col-span-8 desktop:col-span-12 col-span-4">
+        <div className="flex items-center justify-center pt-14">
+          <img src={logovitromex} alt="logo" />
+        </div>
+      </div>
+      <div className="tablet:col-span-6 desktop:col-span-7 col-span-4 row-start-2 text-white">
+        <div>
+          <h1 className="text-title_movil tablet:text-title_table desktop:text-title_desktop">
+            Título
+          </h1>
+          <h2 className="text-subtitle_movil tablet:text-subtitle_table desktop:text-subtitle_desktop">
+            subtitulo
+          </h2>
+          <div className="mt-4">
+            <p className="text-paragraph_movil tablet:text-paragraph_table desktop:text-paragraph_desktop">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Explicabo, veritatis. Impedit cum natus pariatur mollitia
+              praesentium, inventore tenetur qui maxime. Commodi nostrum iste
+              eligendi autem recusandae id, fugit rem minus!
+            </p>
+          </div>
+        </div>
+        <div className="mt-12 flex h-12 w-56 items-center justify-center bg-white text-black">
+          <Link to={Path.LOGIN}>Iniciar sesión</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      movil: "375px",
+      tablet: "600px",
+      desktop: "1240px",
+    },
+    fontSize: {
+      'title_movil': '2.5rem',
+      'title_table': '3.125rem',
+      'title_desktop': '3.75em',
+      'subtitle_movil': '1.25rem',
+      'subtitle_table': '1.563rem',
+      'subtitle_desktop': '1.875rem',
+      'paragraph_movil': '0.875rem',
+      'paragraph_table': '1rem',
+      'paragraph_desktop': '1.25rem',
+    },
+    extend: {
+      colors: { "neutral": "#686868" },
+      gridTemplateColumns: {
+        'movil': 'repeat(4, 25%)',
+        'tableta': 'repeat(8, 12.5%)',
+        'desktop': 'repeat(12, 8.33%)',
+      }
+    },
   },
   plugins: [],
-}
+};
