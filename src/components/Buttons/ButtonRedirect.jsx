d3a57c30f3ddcb98e4a0e064dd37217caeefeca6
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2022-02-14 13:10:59 -0600

    feat: axios add

--- src/components/Buttons/ButtonRedirect.jsx
+++ src/components/Buttons/ButtonRedirect.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { login } from "../../modules/actions/auth";
+import { loginAxios } from "../../modules/actions/auth";
 // import { useNavigate } from 'react-router-dom';
 
 export const ButtonRedirect = ({ text, direction, action = 'login' }) => {
@@ -13,7 +13,7 @@ export const ButtonRedirect = ({ text, direction, action = 'login' }) => {
   // }
   
   const actionForm = () => {
-    dispatch(login('luis@luis.com', 123456))
+    dispatch(loginAxios('luis@luis.com', 123456))
   }
   return (
     <button className="flex h-10 w-52 items-center justify-center bg-white text-black" onClick={actionForm}>
--- src/modules/actions/auth.js
+++ src/modules/actions/auth.js
@@ -1,7 +1,18 @@
+import clientAxios from "../../config/axios"
 import { types } from "../types/types"
 
 
-export const login = (email, password) => {
+export const loginAxios = (email, password) => {
+    return async (dispatch) => {
+        const response = await clientAxios.get('users')
+        console.log(response);
+        // guardar login local store
+        dispatch(login(email))
+    }
+}
+
+
+export const login = (email) => {
     return {
         type: types.login,
         payload: {
