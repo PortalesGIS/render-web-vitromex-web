1b34da01998ffff7aa007a6aa209e16701ac8025
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2022-02-18 09:44:44 -0600

    style: cards h

--- src/components/Cards/CardSeries.jsx
+++ src/components/Cards/CardSeries.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const CardSeries = ({ data }) => {
   return (
     <div className="col-span-2 medium:col-span-3 flex justify-center items-center">
-      <div className="h-full w-full flex flex-col gap-y-2 xsmall:px-2 small:px-2 medium:px-3">
+      <div className="h-full w-full flex flex-col gap-y-2 xsmall:px-2 small:px-2 medium:px-3 cardSeries">
         <div className="rounded-xl overflow-hidden imgCardSeries">
           <img src={data.img} alt="serie" className="h-full w-full object-cover" />
         </div>
