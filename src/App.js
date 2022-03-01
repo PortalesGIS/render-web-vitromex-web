9002cc75aefc384729cee7d49e84534463bf3ebb
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2022-03-01 09:41:57 -0600

    style: v0.40.10 .env

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import { Provider } from "react-redux";
 import { store } from "./modules/store/store";
 import { AppRouter } from "./router/AppRouter";
 import "./App.css";
-import 'animate.css';
+import "animate.css";
 
 function App() {
-  console.log("v0.40.9");
+  let develop = process.env.REACT_APP_VERSION_STATE;
+  develop = develop === "1";
+  console.log(process.env.REACT_APP_VERSION);
   return (
     <>
-      <div className="fixed z-50 bg-blue-100 top-0 left-0">v0.40.9</div>
+      {develop && (
+        <div className="fixed z-50 bg-blue-100 top-0 left-0">
+          {process.env.REACT_APP_VERSION}
+        </div>
+      )}
       <Provider store={store}>
         <AppRouter />
       </Provider>
