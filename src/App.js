5ff92142f2f2d8be0539ac5697d4e99edbdacb03
Luis Enrique Ocampo Gonzalez <blash3g@gmail.com>
2022-03-01 09:42:45 -0600

    style: v0.40.10 siempre aparece

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import "./App.css";
 import "animate.css";
 
 function App() {
-  let develop = process.env.REACT_APP_VERSION_STATE;
-  develop = develop === "1";
+  // let develop = process.env.REACT_APP_VERSION_STATE;
+  // develop = develop === "1";
   console.log(process.env.REACT_APP_VERSION);
   return (
     <>
-      {develop && (
         <div className="fixed z-50 bg-blue-100 top-0 left-0">
           {process.env.REACT_APP_VERSION}
         </div>
-      )}
+      {/* {develop && (
+      )} */}
       <Provider store={store}>
         <AppRouter />
       </Provider>
